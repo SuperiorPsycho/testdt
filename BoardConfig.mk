AlphaDroid-devices/device_xiaomi_surya

surya: Implement torch light control
Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e

[RealJohnGalt:] modify for oneplus usage, add toggle switch
[cyberknight777]: Adapt nodes and SEPolicy for sunny

Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

BoardConfig.mk
@@ -48,6 +48,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.sm6150
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs

SuperiorPsycho/testdt

Signed-off-by: HinohArata <mmgcntk@gmail.com>

BoardConfig.mk
@@ -60,6 +60,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.surya
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs